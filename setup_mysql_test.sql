7ddfa10889e964a618e95c21f89481117dda834f
Arden007 <cloveryuno664@gmail.com>
2023-03-22 11:42:40 +0200

    mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,7 @@
 -- A script that prepares a MySQL server for the project
 -- creates a database, test user, test user's password, grants the test user privileges
 -- grants test user SELECT privileges on database perormance_schema
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
